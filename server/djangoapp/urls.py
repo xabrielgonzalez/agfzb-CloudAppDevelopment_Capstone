de50ec32cfc9a862ac255675f1db9ae6ad385480
Xabriel Gonzalez <109489006+xabrielgonzalez@users.noreply.github.com>
2023-03-25 00:59:48 -0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for about view
     path(route='about/', view=views.about, name='about'),
@@ -16,13 +15,13 @@ urlpatterns = [
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
 
-    # # path for registration
-    # path(route='registration', view=views.registration_request, name='registration'),
-    # # path for login
-    # path(route='login', view=views.login_request, name='login'),
-    # # path for logout
-    # path(route='logout', view=views.logout_request, name='logout'),
-    # path(route='', view=views.get_dealerships, name='index'),
+    path(route='register/', view=views.registration, name='register'),
+    # path for login
+    path(route='login/', view=views.login, name='login'),
+    # path for logout
+    path(route='logout/', view=views.logout, name='logout'),
+    
+    path(route='home/', view=views.home, name='index'),
 
     # path for dealer reviews view
 
